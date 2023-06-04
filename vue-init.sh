c7c51da9c18a415835623dc5cd3b1e9605194052
David Leda <davmail99@yahoo.com>
2023-06-04 09:58:13 -0300

    check dir name

--- vue-init.sh
+++ vue-init.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
 
-# chequear si el usuario agregó el nombre del proyecto
-# si no lo agregó detener la ejecución y mostrar el mensaje de error
-if [ "$#" -eq 0 ]; then
-  echo "Debes agregar el nombre del proyecto luego del script: vue-init.sh nombre-del-proyecto"
-  exit 1
+if [ $# -eq 0 ]; then
+  echo "Debes ingresar el nombre del proyecto."
+else
+  dir="$1"
+
+  if [ -d "./$dir" ]; then
+    
+    cd $dir
+
+    # crear los archivos .env
+    touch .env .env.example
+
+    # en ambos archivos incluir VITE_API_URL
+    echo "VITE_API_URL=" >> .env 
+    echo "VITE_API_URL=" >> .env.example
+
+    # agregar .env a .gitignore
+    echo "# ignore env file" >> .gitignore
+    echo ".env" >> .gitignore
+
+    # borrar todo el contenido del directorio src
+    cd src
+    rm -rf *
+
+    # copiar el nuevo contenido a src
+    cp -a ~/bash-script-for-vue/template/src/* .
+
+    # volver a la raiz del proyecto
+    cd ..
+
+    # instalar deps
+    pnpm install
+
+    # abrir con VS Code
+    code .
+
+    # correr el servidor de desarrollo de vite
+    pnpm run dev
+
+  else
+    echo "No existe un proyecto con el nombre '$dir'."
+  fi
 fi
-
-# el script toma como primer param el dir del proyecto
-cd $1
-
-# crear los archivos .env
-touch .env .env.example
-
-# en ambos archivos incluir VITE_API_URL
-echo "VITE_API_URL=" >> .env 
-echo "VITE_API_URL=" >> .env.example
-
-# agregar .env a .gitignore
-echo "# ignore env file" >> .gitignore
-echo ".env" >> .gitignore
-
-# borrar todo el contenido del directorio src
-cd src
-rm -rf *
-
-# copiar el nuevo contenido a src
-cp -a ~/bash-script-for-vue/template/src/* .
-
-# volver a la raiz del proyecto
-cd ..
-
-# instalar deps
-pnpm install
-
-# abrir con VS Code
-code .
-
-# correr el servidor de desarrollo de vite
-pnpm run dev
-
